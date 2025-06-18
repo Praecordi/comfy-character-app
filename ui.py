3a782ddcec85110a023a672952082ced952d851b
Praecordi <scarlett.stone00@gmail.com>
2025-06-17 22:37:05 -0500

    Fixed character description to avoid error on load

--- ui.py
+++ ui.py
@@ -31,12 +31,15 @@ Eyes Prompt: {eprompt}""".format(
 
 
 def make_character_description(character):
-    char_key = character.lower()
-    char_dict = characters[char_key]
+    if character == "Custom":
+        return ""
+    else:
+        char_key = character.lower()
+        char_dict = characters[char_key]
 
-    desc = "\n".join([f"- **{{{key}}}**" for key in char_dict.keys()])
+        desc = "\n".join([f"- **{{{key}}}**" for key in char_dict.keys()])
 
-    return desc
+        return desc
 
 
 class UI:
