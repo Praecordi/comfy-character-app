6d8d5b8f2cdfaace2fd7069f6deaddec9d8cbe62
Praecordi <scarlett.stone00@gmail.com>
2025-07-01 19:15:29 -0500

    Updated preview mechanism

--- ui/runner.py
+++ ui/runner.py
@@ -1,40 +1,30 @@
 import random
-from queue import Queue, Empty
-from threading import Thread, Event
+from threading import Event, Lock
 import gradio as gr
 
 from comfy_nodes import queue
-from constants import characters
 from workflow.core import CharacterWorkflow
 from workflow.utils import make_output_text
 
 
 class WorkflowRunner:
     def __init__(self):
-        self._preview_queue = Queue()
+        self._preview_lock = Lock()
+        self._latest_preview = None
         self._stop_event = Event()
-        self._thread = None
         self._stop_wf = False
 
     def _push(self, task, node_id, image):
-        self._preview_queue.put(image)
+        with self._preview_lock:
+            self._latest_preview = image
 
-    def _reset_queue(self):
-        self._preview_queue = Queue()
-
-    def _start_preview_processor(self):
-        self._stop_event.clear()
-        self._thread = Thread(target=None, daemon=True)
-        self._thread.start()
-
-    def _stop_preview_processor(self):
-        self._stop_event.set()
-        if self._thread:
-            self._thread.join(timeout=1.0)
+    def _reset_preview(self):
+        with self._preview_lock:
+            self._latest_preview = None
 
     def generate(self, in_state: dict):
-        self._reset_queue()
-        self._start_preview_processor()
+        self._reset_preview()
+        self._stop_event.clear()
 
         in_state["base_seed"] = (
             random.randint(0, 1e10)
@@ -72,7 +62,7 @@ class WorkflowRunner:
         except IndexError:
             yield gr.update(value=res_agg), gr.update()
         finally:
-            self._stop_preview_processor()
+            self._stop_event.set()
 
     def interrupt(self):
         queue.cancel_current()
@@ -81,8 +71,5 @@ class WorkflowRunner:
         if self._stop_event.is_set():
             return (None, "stopped")
 
-        try:
-            image = self._preview_queue.get_nowait()
-            return (image, "running")
-        except Empty:
-            return (None, "running")
+        with self._preview_lock:
+            return (self._latest_preview, "running")
